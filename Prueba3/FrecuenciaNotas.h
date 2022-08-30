3a763fe990df5e7bbaa840e6d6c7cab7aca1c874
CarmenMFM <92932039+CarmenMFM@users.noreply.github.com>
2022-08-30 10:07:30 +0200

    Note frecuency

---
+++ Prueba3/FrecuenciaNotas.h
@@ -0,0 +1,35 @@
+/*
+ * FrecuenciaNotas.h
+ *
+ *  Created on: 25 may. 2022
+ *      Author: carme
+ */
+
+#ifndef FRECUENCIANOTAS_H_
+#define FRECUENCIANOTAS_H_
+
+/*Frecuencia de reloj / frecuencia de la nota   (1MHz/(fnota) */
+
+#define _N__do 3831-1
+#define _N__re 3401-1
+#define _N__mi 3039-1
+#define _N__fa 2865-1
+#define _N__sol 2557-1
+#define _N__solb 2410-1
+#define _N__la 2273-1
+#define _N__lab 2198-1
+#define _N__s 2146-1
+#define _N__doH 1912-1
+#define _N__doSH 1805-1
+#define _N__reH 1703-1
+#define _N__reSH 1608-1
+#define _N__miH 1517-1
+#define _N__faH 1432-1
+#define _N__faSH 1351-1
+#define _N__solH 1275-1
+#define _N__solSH 1205-1
+#define _N__laH 1136-1
+#define _N_N    0
+
+
+#endif /* FRECUENCIANOTAS_H_ */
